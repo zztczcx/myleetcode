91708c00603077e4ee56dd022bca1c9c1b6b29dd
chenxu <zztczcx@163.com>
2016-01-10 23:14:18 +0800

    dd esay count_primes, todo 1.11, tomorrow

---
+++ count_primes.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+# Count the number of prime numbers less than a non-negative number, n.
+# 
+# Hint:
+# 
+# Let's start with a isPrime function. 
+# To determine if a number is prime, 
+# we need to check if it is not divisible by any number less than n. 
+# The runtime complexity of isPrime function would be O(n) and 
+# hence counting the total prime numbers up to n would be O(n2). 
+# Could we do better?
+class Solution(object):
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
