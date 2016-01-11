3ff142b6403233a998b1903d7bc07b596fc3a85d
chenxu <zztczcx@163.com>
2016-01-11 16:05:01 +0800

    add digits, beat  18.98%

---
+++ add_digits.py
@@ -0,0 +1,34 @@
+# -*- coding:utf-8 -*-
+# Given a non-negative integer num,
+# repeatedly add all its digits until the result has only one digit.
+#
+# For example:
+#
+# Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2.
+# Since 2 has only one digit, return it.
+#
+# Follow up:
+# Could you do it without any loop/recursion in O(1) runtime?
+#
+# Hint:
+#
+# A naive implementation of the above process is trivial.
+# Could you come up with other methods?
+# What are all the possible results?
+# How do they occur, periodically or randomly?
+# You may find this Wikipedia article useful.
+
+
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+
+        while num >= 10:
+            num = sum(map(int, list(str(num))))
+        return num
+
+s = Solution()
+print s.addDigits(38)
